94b99a222d8aa4e376894bc49d341447eb82a839
Marcus Asplund <rootfood@gmail.com>
2018-01-15 20:40:29 +0100

    :star: :fire: refactor serviceworker init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import {app} from 'hyperapp'
 import {actions} from './actions/'
 import {state} from './state/'
 import {view} from './views/'
-import {registerWorker} from './utils/register-worker'
 
 window.main = app(state, actions, view, document.getElementById('todo'))
-
-registerWorker()
